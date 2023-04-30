427ccf94f5e5e85f1e00c36dd5433be91925c1d7
Ratnesh <develeopercgproject@gmail.com>
2023-04-30 13:39:28 +0530

    fetch based on page number added

--- src/routes/movies/+page.js
+++ src/routes/movies/+page.js
@@ -1,12 +1,12 @@
-import { PUBLIC_API_kEY } from '$env/static/public'
+// import { PUBLIC_API_kEY } from '$env/static/public'
 
-export async function load({ fetch }) {
+// export async function load({ fetch }) {
 
-  const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${PUBLIC_API_kEY}`
-  const response = await fetch(apiUrl)
-  const data = response.json()
+//   const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${PUBLIC_API_kEY}`
+//   const response = await fetch(apiUrl)
+//   const data = response.json()
 
-  return {
-    data
-  }
-}
+//   return {
+//     data
+//   }
+// }
--- src/routes/movies/+page.svelte
+++ src/routes/movies/+page.svelte
@@ -1,24 +1,42 @@
 <script>
+	import { PUBLIC_API_kEY } from '$env/static/public';
+	import { onMount } from 'svelte';
 
-	export let data;
-	let popularMovie = data.data.results;
-	let pageNumb = data.data.page;
+	let popularMovie = [];
+	let pageNumb = 1;
+	const fetchMovie = async (pageNo) => {
+		let page = pageNo || 1;
+		const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${PUBLIC_API_kEY}&page=${page}`;
+		const response = await fetch(apiUrl);
+		const data = await response.json();
+		pageNumb = data.page;
+		popularMovie = data.results;
+
+		console.log(popularMovie, pageNumb);
+	};
+
+	onMount(() => {
+		fetchMovie(pageNumb);
+	});
 </script>
 
-<section>	
+<section>
 	<div class="container">
 		<div class="movie-page">
 			<div class="page-numbers">
-				<button>Previous</button>
+				<button type="button" on:click={fetchMovie(pageNumb - 1)}>Previous</button>
 				<div class="pagination">
 					<ul>
-						<li><a href={`movie/${pageNumb}`}>{pageNumb}</a></li>
-						<li><a href={`movie/${pageNumb + 1}`}>{pageNumb + 1}</a></li>
-						<li><a href={`movie/${pageNumb + 2}`}>{pageNumb + 2}</a></li>
-						<li><a href={`movie/${pageNumb + 3}`}>{pageNumb + 3}</a></li>
+						<li><button type="button" on:click={fetchMovie(pageNumb)}>{pageNumb}</button></li>
+						<li>
+							<button type="button" on:click={fetchMovie(pageNumb + 1)}>{pageNumb + 1}</button>
+						</li>
+						<li>
+							<button type="button" on:click={fetchMovie(pageNumb + 2)}>{pageNumb + 2}</button>
+						</li>
 					</ul>
 				</div>
-				<button>Next</button>
+				<button on:click={fetchMovie(pageNumb + 1)}>Next</button>
 			</div>
 		</div>
 		<div class="list-container">
