c13ff9422684bc819afd8ca635d8cd5525db91cd
Ratnesh sahu <69114831+alexmercer500@users.noreply.github.com>
2023-05-03 10:34:57 +0000

    changed in mvoies directry

--- src/routes/cast/[castId]/+page.svelte
+++ src/routes/cast/[castId]/+page.svelte
@@ -39,12 +39,12 @@
 			{#each popularMovie as movie}
 				<div class="show-box">
 					<figure>
-						<a href="/movies/{movie.id}">
+						<a href="/movie/{movie.id}">
 							<img src={`http://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
 						</a>
 					</figure>
 					<div class="show-info">
-						<a href="/movies/{movie.id}">{movie.title}</a>
+						<a href="/movie/{movie.id}">{movie.title}</a>
 						<h3>{new Date(movie.release_date).getFullYear()}</h3>
 					</div>
 				</div>
