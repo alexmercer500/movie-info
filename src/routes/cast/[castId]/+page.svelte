8ea8ca0d1d1fa6ca4c38b7d49809611031b37c39
Ratnesh sahu <69114831+alexmercer500@users.noreply.github.com>
2023-05-03 06:23:50 +0000

    test

--- src/routes/cast/[castId]/+page.svelte
+++ src/routes/cast/[castId]/+page.svelte
@@ -1,14 +1,11 @@
 <script>
 	import { PUBLIC_API_kEY } from '$env/static/public';
-  	console.log(PUBLIC_API_kEY);
 	import { onMount } from 'svelte';
 	import { page } from '$app/stores';
-  	console.log($page);
 	let popularMovie = [];
 	let pageNumb = 1;
   	let castId;
   	$:castId = $page.params.castId
-  	$:console.log(castId);
 	const fetchMovie = async (pageNo) => {
 		let page = pageNo || 1;
 		const apiUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${PUBLIC_API_kEY}&sort_by=popularity.desc&page=${pageNumb}&with_cast=${castId}`;
@@ -30,12 +27,8 @@
 				<div class="pagination">
 					<ul>
 						<li><button type="button" on:click={fetchMovie(pageNumb)}>{pageNumb}</button></li>
-						<li>
-							<button type="button" on:click={fetchMovie(pageNumb + 1)}>{pageNumb + 1}</button>
-						</li>
-						<li>
-							<button type="button" on:click={fetchMovie(pageNumb + 2)}>{pageNumb + 2}</button>
-						</li>
+						<li><button type="button" on:click={fetchMovie(pageNumb + 1)}>{pageNumb + 1}</button></li>
+						<li><button type="button" on:click={fetchMovie(pageNumb + 2)}>{pageNumb + 2}</button></li>
 					</ul>
 				</div>
 				<button on:click={fetchMovie(pageNumb + 1)}>Next</button>
